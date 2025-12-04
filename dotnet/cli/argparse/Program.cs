3c0f1e2503475045c887d0f8b779d24ed9b8751b
Steven Tang <yc@steventang.net>
2025-12-04 21:08:14 +1100

    chore: update .net packages

--- dotnet/cli/argparse/Program.cs
+++ dotnet/cli/argparse/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.CommandLine;
-using System.CommandLine.Binding;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
@@ -8,7 +7,6 @@ var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((hostContext, services) =>
     {
         services.AddScoped<TestRunner>();
-        services.AddScoped<TestCommand>();
     })
     .ConfigureLogging(logging =>
     {
@@ -16,58 +14,40 @@ var host = Host.CreateDefaultBuilder(args)
     })
     .Build();
 
-Environment.ExitCode = await host.Services.GetRequiredService<TestCommand>().InvokeAsync(args);
-host.Dispose();
-
-public class TestCommand : RootCommand {
-    private readonly TestRunner _runner;
-
-    public TestCommand(TestRunner runner)
+var rootCommand = new RootCommand("CommandLine Parsing Example");
+var intArg = new Option<int>("--int-arg") { Required = true };
+var stringArg = new Option<string?>("--string-arg");
+rootCommand.Options.Add(intArg);
+rootCommand.Options.Add(stringArg);
+rootCommand.SetAction(parseResult =>
+{
+    var parseTest = new ParseTest
     {
-        _runner = runner;
+        IntArg = parseResult.GetValue(intArg),
+        StringArg = parseResult.GetValue(stringArg)
+    };
 
-        var intArg = new Option<int>("--int-arg") { IsRequired = true };
-        var stringArg = new Option<string?>("--string-arg");
-        this.Description = "CommandLine Parsing Example";
-        this.AddOption(intArg);
-        this.AddOption(stringArg);
-        this.SetHandler(Execute, new ParseTestBinder(intArg, stringArg));
-    }
+    var testRunner = host.Services.GetRequiredService<TestRunner>();
+    return testRunner.Run(parseTest);
+});
 
-    private void Execute(ParseTest test) {
-        _runner.Run(test);
-    }
-}
+var result = await rootCommand.Parse(args).InvokeAsync();
+
+host.Dispose();
+return result;
 
 public class TestRunner(ILogger<TestRunner> logger)
 {
     private readonly ILogger<TestRunner> _logger = logger;
 
-    public void Run(ParseTest parseTest) {
+    public int Run(ParseTest parseTest) {
         _logger.LogInformation("Int: {val}", parseTest.IntArg);
         _logger.LogInformation("String: {val}", parseTest.StringArg);
+        return parseTest.IntArg;
     }
 }
 
 public class ParseTest {
     public int IntArg;
     public string? StringArg;
-}
-
-public class ParseTestBinder : BinderBase<ParseTest> {
-    private readonly Option<int> _intArg;
-    private readonly Option<string?> _stringArg;
-
-    public ParseTestBinder(Option<int> intArg, Option<string?> stringArg) {
-        _intArg = intArg;
-        _stringArg = stringArg;
-    }
-
-    protected override ParseTest GetBoundValue(BindingContext bindingContext)
-    {
-        return new ParseTest {
-            IntArg = bindingContext.ParseResult.GetValueForOption(_intArg),
-            StringArg = bindingContext.ParseResult.GetValueForOption(_stringArg)
-        };
-    }
 }
